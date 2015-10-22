9332489ebc08dcafd605f4f11ffd14201bcee4f5
Evgeny <melnikov_evg@mail.ru>
2015-10-22 08:57:13 +0300

    Create level19.lesson10.home08.Solution.java

---
+++ level19.lesson10.home08.Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.test.level19.lesson10.home08;
+
+/* Перевертыши
+1 Считать с консоли имя файла.
+2 Для каждой строки в файле:
+2.1 переставить все символы в обратном порядке
+2.2 вывести на экран
+3 Закрыть потоки. Не использовать try-with-resources
+
+Пример тела входного файла:
+я - программист.
+Амиго
+
+Пример результата:
+.тсиммаргорп - я
+огимА
+*/
+/*Accepted on JavaRush on 22.10.2015 */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+        while(fileReader.ready()){
+            StringBuilder builder = new StringBuilder(fileReader.readLine());
+            builder = builder.reverse();
+            System.out.println(builder.toString());
+        }
+        reader.close();
+        fileReader.close();
+    }
+}
+
+/* Evgeny JustEM */
